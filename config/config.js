5fbea93f6d4837487807a0448a6836b102062926
Krishna123-cyber <mahatokrishna758@gmail.com>
2024-11-18 15:39:28 -0500

    first commit

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     env: process.env.NODE_ENV || 'development', 
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    mongoUri: process.env.MONGODB_URI ||"mongodb+srv://mahatokrishna758:e6HZhlV4uvt0DqJg@cluster0.cfnea.mongodb.net/Skeleton?retryWrites=true&w=majority&appName=Cluster0mongodb+srv://Blessing:vwze9Nxs4QbbvwzX@cluster0.nfazk.mongodb.net/Skeleton?retryWrites=true&w=majority&appName=Cluster0"||
+    mongoUri: process.env.MONGODB_URI ||"mongodb+srv://mahatokrishna758:e6HZhlV4uvt0DqJg@cluster0.cfnea.mongodb.net/Skeleton?retryWrites=true&w=majority&appName=Cluster0"||
     process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' + 
    (process.env.MONGO_PORT || '27017') +
